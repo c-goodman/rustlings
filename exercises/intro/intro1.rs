110bf8f44566950735a3a421c5c19ba86bfa8ba5
c-goodman <cbgoodm@gmail.com>
2022-12-12 20:37:26 -0500

    Finish inital rustlings

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -27,4 +27,5 @@ fn main() {
     println!();
     println!("The source for this exercise is in `exercises/intro/intro1.rs`. Have a look!");
     println!("Going forward, the source of the exercises will always be in the success/failure output.");
+    println!("Hello Rustlings!");
 }
